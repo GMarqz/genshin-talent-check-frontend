7d7a3bae3ad005ba44cc5b25e715187f9f851cea
GMarqz <gabrielmarques9782@gmail.com>
2024-12-06 16:13:59 -0300

    add code that was previous in createModal.ts

---
+++ app/components/modal/index.ts
@@ -0,0 +1,41 @@
+import { character } from './../../mockDB';
+import createInputSelect from "./modalInputSelect.js";
+
+export default function createModal(parentElement: HTMLElement, character: character): HTMLDivElement{
+    const $cardModal = document.createElement("DIV") as HTMLDivElement;
+    $cardModal.className = "card-modal";
+    $cardModal.id = "card-modal";
+    parentElement.appendChild($cardModal);
+
+    $cardModal.innerHTML = `
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>${character.name}</h5>
+                <button type="button" class="modal-header-btn" id="modal-btn-close" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body" id="modal-body">
+                <p class="modal-character-description">${character.description}</p>
+                ${setTimeout(() => {
+                    createInputSelect("Normal Attack", character.talents.normalAtk)
+                }, 200)}
+                ${setTimeout(() => {
+                    createInputSelect("Elemental Skill", character.talents.elementalSkill)
+                }, 200)}
+                ${setTimeout(() => {
+                    createInputSelect("Elemental Burst", character.talents.elementalBurst)
+                }, 200)}
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="modal-btn" id="modal-btn-close">Close</button>
+                <button type="button" class="modal-btn" id="modal-btn-save">Save changes</button>
+            </div>
+        </div>
+    `;
+
+    return $cardModal;
+// Link referencia para modal: https://www.w3schools.com/howto/howto_css_modals.asp
+// Link de referencia para o modal: https://getbootstrap.com/docs/5.3/components/modal/
+// Link de referencia para o input select do modal: https://getbootstrap.com/docs/5.3/forms/input-group/
+}
